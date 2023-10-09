8b223eb7a06d636d944e29378bbf244c5a03df05
Real <guutaahassen2@gmail.com>
2023-10-09 13:09:23 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+import express from "express";
+
+const app = express();
+const port = 3000;
+// uses static method to access non dynamic files (css,images and all files that are not changing)
+app.use(express.static("public"));
+
+// below is rendering ejs files
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.get("/myPortfolio", (req,res)=>{
+  res.render("myPortfolio.ejs")
+})
+
+app.get("/services", (req,res)=>{
+  res.render("service.ejs")
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
